0b6eae804a877bc6274bd8e73881c7c8c9716c74
kanderson425 <kyleanderson.dev@gmail.com>
2021-01-06 23:03:01 -0600

    Adds the ability to serve up HTML files using the path core node module.

--- node-basic-app/app.js
+++ node-basic-app/app.js
@@ -1,20 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs!");
-  next();
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from 'Add-Product' Page</h1>");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express</h1>");
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
