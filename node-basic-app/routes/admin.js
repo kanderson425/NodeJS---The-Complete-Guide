fd0e1c098bf8d9d7e36dce9f5335d4f1d3684926
kanderson425 <kyleanderson.dev@gmail.com>
2021-01-09 17:26:51 -0600

    Updates the add-product.html file to an add-product.pug file.

--- node-basic-app/routes/admin.js
+++ node-basic-app/routes/admin.js
@@ -10,7 +10,8 @@ const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product");
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
