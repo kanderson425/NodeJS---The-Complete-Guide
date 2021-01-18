af2e28919620ca3292ef85f840004fc2dbdaa186
kanderson425 <kyleanderson.dev@gmail.com>
2021-01-17 21:14:21 -0600

    Adds dynami routes for specific product details pages.

--- node-basic-app/controllers/shop.js
+++ node-basic-app/controllers/shop.js
@@ -20,6 +20,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
+};
+
 exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     pageTitle: "Cart",
--- node-basic-app/routes/shop.js
+++ node-basic-app/routes/shop.js
@@ -6,19 +6,16 @@ const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-// / => GET
 router.get("/", shopController.getIndex);
 
-// /products => GET
 router.get("/products", shopController.getProducts);
 
-// /cart => GET
+router.get("/products/:productId", shopController.getProduct);
+
 router.get("/cart", shopController.getCart);
 
-// /orders => GET
 router.get("/orders", shopController.getOrders);
 
-// /checkout => GET
 router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
