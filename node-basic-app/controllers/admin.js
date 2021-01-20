bd0ec7dfa59abc2d5826f8181e21967866beaf17
kanderson425 <kyleanderson.dev@gmail.com>
2021-01-19 21:39:11 -0600

    Adds the delete method on the cart and product model.

--- node-basic-app/controllers/admin.js
+++ node-basic-app/controllers/admin.js
@@ -54,6 +54,12 @@ exports.postEditProduct = (req, res, next) => {
   res.redirect("/admin/products");
 };
 
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect("/admin/products");
+};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("admin/products", {
