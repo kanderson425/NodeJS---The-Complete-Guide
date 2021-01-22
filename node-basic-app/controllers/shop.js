a39966b9961140cc18a0f69542a26852d6257bb0
kanderson425 <kyleanderson.dev@gmail.com>
2021-01-21 23:07:32 -0600

    Adds the postCartDeleteProduct route to delete products from the cart.

--- node-basic-app/controllers/shop.js
+++ node-basic-app/controllers/shop.js
@@ -61,6 +61,14 @@ exports.postCart = (req, res, next) => {
   res.redirect("/cart");
 };
 
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     pageTitle: "Your Orders",
