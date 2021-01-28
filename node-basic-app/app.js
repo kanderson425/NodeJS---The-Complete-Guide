ea6ed335e2615e35816740d169b0d2e70de898bc
kanderson425 <kyleanderson.dev@gmail.com>
2021-01-27 22:22:32 -0600

    Adds successful integration of sequelize and sucessful product insertion into the products table.

--- node-basic-app/app.js
+++ node-basic-app/app.js
@@ -4,7 +4,7 @@ const expressHbs = require("express-handlebars");
 const path = require("path");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -22,14 +22,6 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-// db.execute("SELECT * FROM products")
-//   .then((result) => {
-//     console.log(result[0], result[1]);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -38,4 +30,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
