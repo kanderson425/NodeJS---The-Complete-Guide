8887b9a2c5e38a9d2f25f1d5ccae1278c83386f1
kanderson425 <kyleanderson.dev@gmail.com>
2021-02-09 08:26:20 -0600

    Adds the completed fetchById functionality/see individual product details page.

--- node-basic-app/models/product.js
+++ node-basic-app/models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -30,6 +31,21 @@ class Product {
       .then((products) => {
         console.log(products);
         return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
       })
       .catch((err) => {
         console.log(err);
