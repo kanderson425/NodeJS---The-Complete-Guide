8543ff391d48fee959c20aa14d310a8ae7be48e6
kanderson425 <kyleanderson.dev@gmail.com>
2021-03-18 16:09:11 -0500

    Creates the rest_api_project folder with a basic get and post request.

---
+++ rest_api_project/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const feedRoutes = require("./routes/feed");
+
+const app = express();
+
+// app.use(bodyParser.urlencoded());  // x-ww-form-urlencoded <form>
+app.use(bodyParser.json()); //application/json
+
+app.use("/feed", feedRoutes);
+
+app.listen(8080);
