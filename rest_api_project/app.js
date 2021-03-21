237fdd4360b13936e77fd065c37d2fb5265b38de
kanderson425 <kyleanderson.dev@gmail.com>
2021-03-20 23:57:40 -0500

    Creates the Post schema and sets up Mongodb connection for Post creation.

--- rest_api_project/app.js
+++ rest_api_project/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
@@ -8,6 +9,23 @@ const app = express();
 // app.use(bodyParser.urlencoded());  // x-ww-form-urlencoded <form>
 app.use(bodyParser.json()); //application/json
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    "mongodb+srv://kanderson425:Test1234@cluster0.qjf8n.mongodb.net/messages"
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
--- rest_api_project/controllers/feed.js
+++ rest_api_project/controllers/feed.js
@@ -1,20 +1,48 @@
+const { body } = require("express-validator/check");
+
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: "1",
         title: "First Post",
         content: "This is the first post",
+        imageUrl: "images/Funny_Cat.jpg",
+        creator: {
+          name: "Kyle Anderson",
+        },
+        createdAt: new Date(),
       },
     ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array(),
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+  const post = new Post({
+    title: title,
+    content: content,
+    creator: { name: "Kyle" },
   });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
