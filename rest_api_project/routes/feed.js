d9caf8048090b43e9b0b4704de5e3bc2950e12f9
kanderson425 <kyleanderson.dev@gmail.com>
2021-03-28 23:22:12 -0500

    Adds server side validation to the getPosts route to check for a present session token.

--- rest_api_project/routes/feed.js
+++ rest_api_project/routes/feed.js
@@ -2,11 +2,12 @@ const express = require("express");
 const { body } = require("express-validator/check");
 
 const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/isAuth");
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post(
